e381bb9fc3e7d0e48873418a941b6c93cb64018d
ashniv05 <148707601+ashniv05@users.noreply.github.com>
2024-09-24 12:29:05 -0400

    Update my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -8,19 +8,49 @@ import { LitElement, html, css } from 'lit';
 
 export class MyCard extends LitElement {
 
-  static get tag() {
-    return 'my-card';
-  }
+  static get properties() {
+      return {
+        title: { type: String
+
+         },
+        image: { type: String 
+
+        },
+        description: { type: String 
+
+        },
+        link: { type: String 
+
+        }
+      };
+    }
+  
 
   constructor() {
     super();
     this.title = "My card";
+    this.description = 'Description';
+    this.image = '';
+    this.link = '#';
+    }
   }
 
   static get styles() {
     return css`
-      :host {
-        display: block;
+      .card {
+        border: 1px solid #ccc;
+        padding: 16px;
+        background-color: white;
+      }
+      .card img {
+        max-width: 100%;
+      }
+      .card h3 {
+        margin: 0;
+      }
+      .card a {
+        color: blue;
+        text-decoration: none;
       }
     `;
   }
